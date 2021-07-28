7d2ca2ad76be6e28e62148be9b0a8c2cf8b5e7db
RosiDeyanova <rositsa.deyanova@gmail.com>
2021-07-28 21:28:24 +0300

    started creating the views

---
+++ First ASP.NET App/Controllers/PieController.cs
@@ -0,0 +1,27 @@
+﻿using First_ASP.NET_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_ASP.NET_App.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List() 
+        {
+            return View(_pieRepository.AllPies);
+        }
+
+    }
+}
