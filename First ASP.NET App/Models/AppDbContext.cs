5c566e571a2e0e571d6f3fef2b6287d32228f0d5
RosiDeyanova <rositsa.deyanova@gmail.com>
2021-07-29 16:19:51 +0300

    configured database

---
+++ First ASP.NET App/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_ASP.NET_App.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Pie> Pies { set; get; }
+        public DbSet<Category> Categories { set; get; }
+    }
+}
